cebdb85621021b4fb4df3f8062287cd76ecbbc46
Diego Urrego <diegourrg@gmail.com>
2024-03-01 21:04:38 -0500

    feat: homework done.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 // import './topics/01-basic-types.ts' --> Este es un módulo
 // import './topics/02-object-interface.ts'
-import './topics/03-functions.ts'
+// import './topics/03-functions.ts'
+import './topics/04-homeworktypes.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   Hola Mundo
