3388af9e8c74665daf18e3c0a3eb3b1ef69b7116
Diego Urrego <diegourrg@gmail.com>
2024-03-03 15:44:06 -0500

    feat: classes and its build understood.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import './style.css'
+import "./style.css";
 // import './topics/01-basic-types.ts' --> Este es un módulo
 // import './topics/02-object-interface.ts'
 // import './topics/03-functions.ts'
 // import './topics/04-homeworktypes.ts'
 // import './topics/05-basic-destructuring.ts'
 // import './topics/06-function-destructuring.ts'
-import './topics/07-import-exports.ts'
+// import './topics/07-import-exports.ts'
+import "./topics/08-classes.ts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   Hola Mundo
-`
+`;
